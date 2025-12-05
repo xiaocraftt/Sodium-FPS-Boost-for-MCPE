2a9fa7962907be5cac484100705aba2c5d39743e
XiaoCraft <konkarprem@gmail.com>
2025-12-05 21:54:10 +0530

    Add files via upload

---
+++ shaders/hlsl/selection_box.fragment.hlsl
@@ -0,0 +1,20 @@
+#include "ShaderConstants.fxh"
+struct PS_Input
+{
+	float4 position : SV_Position;
+};
+struct PS_Output
+{
+float4 color : SV_Target;
+#ifdef FORCE_DEPTH_ZERO
+float depth : SV_Depth;
+#endif
+};
+ROOT_SIGNATURE
+void main(in PS_Input PSInput, out PS_Output PSOutput)
+{
+PSOutput.color = float4(100.0,100.0,100.0,100.0);
+#ifdef FORCE_DEPTH_ZERO
+	PSOutput.depth = 10.0;
+#endif
+}
